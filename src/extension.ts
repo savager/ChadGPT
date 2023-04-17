6b2f556d8044f3b5b4e881d563f4218807541509
Russ Savage <savage.russel@gmail.com>
2023-04-17 14:26:10 -0500

    chore: super dumb v1

--- src/extension.ts
+++ src/extension.ts
@@ -1,105 +1,204 @@
 /* eslint-disable @typescript-eslint/semi */
 /* eslint-disable @typescript-eslint/naming-convention */
-import * as vscode from 'vscode';
-import { Configuration, OpenAIApi } from 'openai';
+import * as vscode from 'vscode'
+import { Configuration, OpenAIApi } from 'openai'
+import { TextEditor } from 'vscode'
 
-let openai: any;
+let openai: any
+let defaultModel: string = 'gpt-3.5-turbo'
 
 async function setApiKey(apiKey: string): Promise<void> {
   if (!isValidApiKey(apiKey)) {
-    vscode.window.showErrorMessage(`Error: OpenAI API Key Format Invalid. Not Saved`);
-    return;
+    vscode.window.showErrorMessage(`Error: OpenAI API Key Format Invalid. Not Saved`)
+    return
   } else {
-    const config = vscode.workspace.getConfiguration('chadgpt');
-    await config.update('apiKey', apiKey, vscode.ConfigurationTarget.Global);
-    vscode.window.showInformationMessage('API Key has been set.');
-    const configuration = new Configuration({
-      apiKey
-    })
-    openai = new OpenAIApi(configuration)
+    const config = vscode.workspace.getConfiguration('chadgpt')
+    await config.update('apiKey', apiKey, vscode.ConfigurationTarget.Global)
+    vscode.window.showInformationMessage('API Key has been set.')
+    openai = new OpenAIApi(
+      new Configuration({
+        apiKey
+      })
+    )
   }
 }
 
+async function setModel(modelName: string): Promise<void> {
+  const config = vscode.workspace.getConfiguration('chadgpt')
+  await config.update('model', modelName, vscode.ConfigurationTarget.Global)
+  vscode.window.showInformationMessage('OpenAI model name has been set.')
+}
+
+
 async function getApiKey(): Promise<string | undefined> {
-  const config = vscode.workspace.getConfiguration('chadgpt');
-  return config.get('apiKey');
+  const config = vscode.workspace.getConfiguration('chadgpt')
+  return config.get('apiKey')
+}
+
+
+async function getModel(): Promise<string | undefined> {
+  const config = vscode.workspace.getConfiguration('chadgpt')
+  return config.get('model')
 }
 
 async function fetchResponse(prompt: string): Promise<string> {
-  const apiKey = await getApiKey();
+  return vscode.window.withProgress(
+    {
+      location: vscode.ProgressLocation.Window,
+      cancellable: false,
+      title: 'ChatGPT generating response...'
+    },
+    async progress => {
 
-  if (!apiKey) {
-    throw new Error('API Key not set.');
-  }
+      progress.report({ increment: 0 })
 
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    max_tokens: 2048,
-    n: 1,
-    temperature: 0.5,
-    messages: [{ role: 'user', content: prompt }],
-  });
+      const apiKey = await getApiKey()
+      const model = await getModel()
 
-  if (!response) {
-    throw new Error('No response received.');
-  }
+      if (!apiKey) {
+        throw new Error('API Key not set.')
+      }
+    
+      const response = await openai.createChatCompletion({
+        model,
+        n: 1,
+        temperature: 0.5,
+        messages: [{ role: 'user', content: prompt }]
+      })
+    
+      if (!response) {
+        throw new Error('No response received.')
+      }
+    
+      progress.report({ increment: 100 })
+      
+      return response.data.choices[0].message.content.trim()
 
-  return response.data.choices[0].message.content.trim();
+    }
+  )
 }
 
 function isValidApiKey(apiKey: string | undefined): boolean {
-  if (!apiKey) {return false;}
-  const apiKeyPattern = /^sk-[A-Za-z0-9-]{48}$/;
-  return apiKeyPattern.test(apiKey);
+  if (!apiKey) {
+    return false
+  }
+  const apiKeyPattern = /^sk-[A-Za-z0-9-]{48}$/
+  return apiKeyPattern.test(apiKey)
 }
 
-export function activate(context: vscode.ExtensionContext) {
+function hasSelection(editor: TextEditor): Boolean {
+  if (editor) {
+    const selections = editor.selections
+    let hasHighlightedText = false
 
-	const outputChannel = vscode.window.createOutputChannel('ChadGPT');
+    selections.forEach(selection => {
+      if (!selection.isEmpty) {
+        hasHighlightedText = true
+      }
+    })
+
+    if (hasHighlightedText) {
+      vscode.window.showInformationMessage('Highlighted text in editor will be included in query.')
+      return true
+    } else {
+      return false
+    }
+  } else {
+    return false
+  }
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+
+  const outputChannel = vscode.window.createOutputChannel('ChadGPT')
+
+  setModel(defaultModel)
+
+  const key : string | undefined = await getApiKey()
+  const isValid = isValidApiKey(key)
+
+  if (typeof key !== 'undefined' && isValid) {
+    openai = new OpenAIApi(
+      new Configuration({
+        apiKey: key
+      })
+    )
+  }
 
   context.subscriptions.push(
     vscode.commands.registerCommand('chadgpt.askChatGPT', async () => {
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        return;
-      }
-
+      
       const userInput = await vscode.window.showInputBox({
-        placeHolder: 'Type your question here',
-      });
-
+        placeHolder: 'Type your question here'
+      })
+      
       if (!userInput) {
-        return;
+        return
       }
+      
+      let prompt: string = ''
+      const editor = vscode.window.activeTextEditor
+      
+      if (typeof editor === 'undefined' || editor.document?.uri?.path.includes('extension-output')) {
+        prompt = `${userInput}`
+      } else {
+        const selection = hasSelection(editor)
 
-      const activeFileContent = editor.document.getText();
-      const prompt = `${userInput}\n\n${activeFileContent}`;
+        if (selection) {
+          prompt = `${userInput}\n`
+          const selections = editor.selections
+          let document = editor.document
+
+          selections.forEach(selection => {
+            if (!selection.isEmpty) {
+              const selectedText = document.getText(selection)
+              prompt = prompt + selectedText
+            }
+          })
+        } else {
+          const activeFileContent = editor.document.getText() || ''
+          prompt = `${userInput}\n${activeFileContent}`
+        }
+      }
 
       try {
-				
-				const response = await fetchResponse(prompt);
-				outputChannel.appendLine(response);
-				outputChannel.show();
-
+        const response = await fetchResponse(prompt)
+        outputChannel.appendLine(response)
+        outputChannel.appendLine('\n')
+        outputChannel.show()
       } catch (error: any) {
-        vscode.window.showErrorMessage(`Error: ${error.message}`);
+        vscode.window.showErrorMessage(`Error: ${error.message}`)
       }
     })
-  );
+  )
 
   context.subscriptions.push(
     vscode.commands.registerCommand('chadgpt.setApiKey', async () => {
       const apiKeyInput = await vscode.window.showInputBox({
-        placeHolder: 'Enter your OpenAI API Key',
-      });
+        placeHolder: 'Enter your OpenAI API Key'
+      })
 
       if (!apiKeyInput) {
-        return;
+        return
       }
 
-      await setApiKey(apiKeyInput);
+      await setApiKey(apiKeyInput)
     })
-  );
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('chadgpt.setAIModel', async () => {
+      const modelName = await vscode.window.showInputBox({
+        placeHolder: `gpt-3.5-turbo`
+      })
+
+      if (!modelName) {
+        return
+      }
+
+      await setModel(modelName)
+    })
+  )
 }
 
 export function deactivate() {}
